c1f88770c6855582783ee78b46b2e69b63cc2a46
Lesego864 <tshipulesego@gmail.com>
2021-08-11 20:58:02 +0200

    initial changes

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -10,3 +10,38 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+const radioBillAddBtnElement = document.querySelector(".radioBillAddBtn");
+const rCallsTotalElement = document.querySelector(".callTotalTwo");
+const rSmsTotalElement = document.querySelector(".smsTotalTwo");
+const totalCostElement = document.querySelector(".orange");
+
+var radioCallstotal = 0;
+var radioSmstotal = 0;
+function radioBillTotal() {
+    const checkedRadioBtn = document.querySelector(".billItemTypeRadio:checked");
+    var billItemType = checkedRadioBtn.value;
+    if (billItemType == "call") {
+        radioCallstotal += 2.75;
+    }
+    else if (billItemType == "sms") {
+        radioSmstotal += 0.75;
+    }
+
+    rcallsTotalElem.innerHTML = radioCallstotal.toFixed(2);
+    rsmsTotalElem.innerHTML = radioSmstotal.toFixed(2);
+    var radiototalCost = radioCallstotal + radioSmstotal;
+    var number = radiototalCost.toFixed(2);
+    
+
+    if (radiototalCost >= 30 && radiototalCost < 50) {
+        rtotalCostElem.classList.add("warning");
+    }
+    else if (radiototalCost >= 50) {
+        rtotalCostElem.classList.add("danger");
+        number=50;
+    }
+    rtotalCostElem.innerHTML = "R" + number;
+}
+
+radioTotalAddBtn.addEventListener('click', radioBillTotal);
--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,40 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+
+const billTypeTextElement = document.querySelector(".billTypeText");
+const addToBillBtnElement = document.querySelector(".addToBillBtn");
+const callTotalOneElement = document.querySelector(".callTotalOne");
+const smsTotalOneElement = document.querySelector(".smsTotalOne");
+const totalOneElement = document.querySelector(".red");
+
+var callsTotal = 0;
+var smsTotal = 0;
+
+function textBillTotal() {
+    var billTypeEntered = billTypeText.value.trim();
+
+    if (billTypeEntered === "call"|| billTypeEntered === "Call" || billTypeEntered === "CALL") {
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms" || billTypeEntered === "Sms" || billTypeEntered === "SMS") {
+        smsTotal += 0.75;
+    }
+
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+
+
+    if (totalCost >= 30 && totalCost < 50) {
+        totalCostElem.classList.add("warning");
+    }
+    else if (totalCost >= 50) {
+        totalCostElem.classList.add("danger");
+    }
+    totalOneElement.innerHTML = "R" + number;
+};
+
+textTotalAddBtn.addEventListener('click', textBillTotal);
